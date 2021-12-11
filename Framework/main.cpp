fa54097e92ba61a0702b0462780ca5054f01e26a
mptf <moritz.potthoff@gmx.de>
2021-12-11 13:17:24 +0100

    Include basic input logic.

--- Framework/main.cpp
+++ Framework/main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <fstream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        std::cout << "Wrong number of arguments, expecting 2 arguments." << std::endl;
+        return 1;
+    }
+
+    std::string queryChoice(argv[1]);
+    std::string inputFileName(argv[2]);
+
+    std::ifstream inputFile;
+    inputFile.open(inputFileName);
+    if (!inputFile.is_open()) {
+        std::cout << "Error while reading file." << std::endl;
+        return 1;
+    }
+    std::cout << "Reading input file" << std::endl;
+    char line[100];
+    while (inputFile.getline(line, 100)) {
+        std::cout << line << std::endl;
+    }
+    inputFile.close();
+
+    if (queryChoice.compare("topk") == 0) {
+        std::cout << "Requested topk query." << std::endl;
+    } else if (queryChoice.compare("repeat") == 0) {
+        std::cout << "Requested repeat query." << std::endl;
+    } else {
+        std::cout << "Unknown query choice." << std::endl;
+        return 1;
+    }
+
     return 0;
 }
