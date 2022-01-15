a4da9ac8c62386191cde694b2a4ab38a634e3d9b
mptf <moritz.potthoff@gmx.de>
2022-01-15 23:23:16 +0100

    Cleanup main file, add documentation, generate the desired output lines.

--- Framework/main.cpp
+++ Framework/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <chrono>
 
 #include "NaiveSuffixTree/SuffixTree.h"
 #include "Query/TopKQuery.h"
@@ -8,10 +7,131 @@
 #include "Helpers/Timer.h"
 #include "Helpers/TopKProfiler.h"
 
+/**
+ * One topK Query for length l and the k-th candidate.
+ */
 struct TopKQuery {
     size_t l;
     size_t k;
 };
+
+//Debug flag.
+static const bool Debug = false;
+
+inline static void readRemainingFileContents(std::ifstream& inputFile, std::string& inputText) {
+    std::stringstream inputBuffer;
+    inputBuffer << inputFile.rdbuf();
+    inputText = inputBuffer.str();
+}
+
+inline static void handleTopKQuery(char *argv[]) {
+    if constexpr (Debug) std::cout << "Requested topk query." << std::endl;
+
+    std::string inputFileName(argv[2]);
+    std::ifstream inputFile(inputFileName);
+
+    //Read the queries from the file.
+    size_t numberOfQueries;
+    inputFile >> numberOfQueries;
+    std::vector<TopKQuery> queries;
+    queries.reserve(numberOfQueries);
+    for (size_t i = 0; i < numberOfQueries; i++) {
+        size_t k, l;
+        inputFile >> l;
+        inputFile >> k;
+        queries.emplace_back(l, k);
+    }
+    if constexpr (Debug) std::cout << "Found " << numberOfQueries << " queries." << std::endl;
+
+    //Read the actual text.
+    std::string inputText;
+    readRemainingFileContents(inputFile, inputText);
+
+    //Used for time for the output.
+    Helpers::Timer preprocessingTimer;
+    //Generate the suffix tree for the input.
+    //Use +/-2 in start and length to cut off the line break between the last query part and the actual text.
+    NaiveSuffixTree::SuffixTree<char, Debug> stree(inputText.c_str() + 2, inputText.length() - 2);
+    size_t preprocessingTime = preprocessingTimer.getMilliseconds();
+    if constexpr (Debug) std::cout << "Generated suffix tree for input: '" << stree.text << "'" << std::endl;
+
+    //The time needed (once) for additional query preprocessing will be added to the suffix tree generation time for the total preprocessing time.
+    Helpers::Timer queryInitTimer;
+    //Generate the query instance.
+    Query::TopKQuery<char, Query::TopKProfiler, Debug> query(&stree);
+    size_t queryInitTime = queryInitTimer.getMilliseconds();
+
+    size_t totalQueryTime = 0;
+    Helpers::Timer queryTimer;
+    //Run the queries.
+    std::stringstream queryResults;
+    for (size_t i = 0; i < numberOfQueries; i++) {
+        queryTimer.restart();
+        size_t startIndex = query.runQuery(queries[i].l, queries[i].k);
+        totalQueryTime += queryTimer.getMilliseconds();
+        queryResults << stree.substring(startIndex, queries[i].l);
+        if (i < numberOfQueries - 1) queryResults << ";";
+        std::cout << "Query l=" << queries[i].l << ", k=" << queries[i].k << ": " << stree.substring(startIndex, queries[i].l) << " (" << startIndex << ")" << std::endl;
+    }
+
+    std::cout << "Preprocessing time: " << preprocessingTime << std::endl;
+    std::cout << "Query init. time:   " << queryInitTime << std::endl;
+    std::cout << "Total query time:   " << totalQueryTime << std::endl;
+    std::cout << "Avg. query time:    " << totalQueryTime / (double) numberOfQueries << std::endl;
+
+    std::cout << std::endl;
+    query.profiler.print();
+    std::cout << std::endl;
+
+    std::cout   << "RESULT algo=topk name=moritz-potthoff"
+                << " construction time=" << (preprocessingTime + queryInitTime)
+                << " query time=" << totalQueryTime
+                << " solutions=" << queryResults.str()
+                << " file=" << inputFileName << std::endl;
+}
+
+inline static void handleRepeatQuery(char *argv[]) {
+    std::cout << "Requested repeat query." << std::endl;
+
+    std::string inputFileName(argv[2]);
+    std::ifstream inputFile(inputFileName);
+
+    //Read the input text.
+    std::string inputText;
+    readRemainingFileContents(inputFile, inputText);
+    if constexpr (Debug) std::cout << "Read input file: '" << inputText << "'" << std::endl;
+
+    //Measure the preprocessing time.
+    Helpers::Timer preprocessingTimer;
+    //Generate the suffix tree.
+    NaiveSuffixTree::SuffixTree<char, Debug> stree(inputText.c_str(), inputText.length());
+    size_t preprocessingTime = preprocessingTimer.getMilliseconds();
+
+    //Again, query initialization time will be measured as preprocessing time.
+    Helpers::Timer queryInitTimer;
+    //Generate the query instance.
+    Query::RepeatQuery<char, Debug> query(&stree);
+    size_t queryInitTime = queryInitTimer.getMilliseconds();
+
+    size_t startPosition, length;
+    Helpers::Timer queryTimer;
+    //Compute the query.
+    std::tie(startPosition, length) = query.runQuery();
+    size_t queryTime = queryTimer.getMilliseconds();
+    std::cout << "Query result: " << stree.substring(startPosition, length) << stree.substring(startPosition, length) << " (" << startPosition << ", " << length << ")" << std::endl << std::endl;
+
+    std::cout << "Preprocessing time: " << preprocessingTime << std::endl;
+    std::cout << "Query init. time:   " << queryInitTime << std::endl;
+    std::cout << "Total query time:   " << queryTime << std::endl;
+
+    std::cout << std::endl << std::endl;
+
+    std::cout << "RESULT algo=repeat name=moritz-potthoff"
+              << " construction time=" << (preprocessingTime + queryInitTime)
+              << " query time=" << queryTime
+              << " solutions=" << stree.substring(startPosition, length)
+              << " file=" << inputFileName << std::endl;
+}
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
@@ -19,95 +139,11 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    const bool Debug = false;
-    const bool PreprocessingDebug = false;
-
     std::string queryChoice(argv[1]);
     if (queryChoice.compare("topk") == 0) {
-        if constexpr (Debug) std::cout << "Requested topk query." << std::endl;
-
-        std::string inputFileName(argv[2]);
-        std::ifstream inputFile(inputFileName);
-
-        size_t numberOfQueries;
-        inputFile >> numberOfQueries;
-        std::vector<TopKQuery> queries;
-        queries.reserve(numberOfQueries);
-        //read all the queries
-        for (size_t i = 0; i < numberOfQueries; i++) {
-            size_t k, l;
-            inputFile >> l;
-            inputFile >> k;
-            queries.emplace_back(l, k);
-        }
-        if constexpr (Debug) std::cout << "Found " << numberOfQueries << " queries." << std::endl;
-
-        //read remaining part of the input
-        std::stringstream inputBuffer;
-        inputBuffer << inputFile.rdbuf();
-        std::string inputText = inputBuffer.str();
-
-        Helpers::Timer preprocessingTimer;
-        NaiveSuffixTree::SuffixTree<char, PreprocessingDebug> stree(inputText.c_str() + 2, inputText.length() - 2); //+/-2 to skip the new line that is still read into inputText
-        size_t preprocessingTime = preprocessingTimer.getMilliseconds();
-        std::cout << "Generated suffix tree for input: '" << stree.text << "'" << std::endl;
-
-        //generate query
-        Helpers::Timer queryInitTimer;
-        Query::TopKQuery<char, Query::TopKProfiler, Debug> query(&stree);
-        size_t queryInitTime = queryInitTimer.getMilliseconds();
-
-        size_t totalQueryTime = 0;
-        Helpers::Timer queryTimer;
-        //run queries
-        for (size_t i = 0; i < numberOfQueries; i++) {
-            queryTimer.restart();
-            size_t startIndex = query.runQuery(queries[i].l, queries[i].k);
-            totalQueryTime += queryTimer.getMilliseconds();
-            std::cout << "Query l=" << queries[i].l << ", k=" << queries[i].k << ": " << stree.substring(startIndex, queries[i].l) << " (" << startIndex << ")" << std::endl;
-        }
-
-        std::cout << "Preprocessing time: " << preprocessingTime << std::endl;
-        std::cout << "Query init. time:   " << queryInitTime << std::endl;
-        std::cout << "Total query time:   " << totalQueryTime << std::endl;
-        std::cout << "Avg. query time:    " << totalQueryTime / (double) numberOfQueries << std::endl;
-
-        std::cout << std::endl;
-        query.profiler.print();
+        handleTopKQuery(argv);
     } else if (queryChoice.compare("repeat") == 0) {
-        std::cout << "Requested repeat query." << std::endl;
-
-        std::string inputFileName(argv[2]);
-        std::ifstream inputFile(inputFileName);
-
-        //read remaining part of the input
-        /*
-        std::stringstream inputBuffer;
-        inputBuffer << inputFile.rdbuf();
-        std::string inputText = inputBuffer.str();
-        */
-        std::string inputText;
-        inputFile >> inputText;
-        if constexpr (Debug) std::cout << "Read input file: '" << inputText << "'" << std::endl;
-
-        Helpers::Timer preprocessingTimer;
-        NaiveSuffixTree::SuffixTree<char, PreprocessingDebug> stree(inputText.c_str(), inputText.length());
-        size_t preprocessingTime = preprocessingTimer.getMilliseconds();
-
-        //generate query
-        Helpers::Timer queryInitTimer;
-        Query::RepeatQuery<char, Debug> query(&stree);
-        size_t queryInitTime = queryInitTimer.getMilliseconds();
-
-        size_t startPosition, length;
-        Helpers::Timer queryTimer;
-        std::tie(startPosition, length) = query.runQuery();
-        size_t queryTime = queryTimer.getMilliseconds();
-        std::cout << "Query result: " << stree.substring(startPosition, length) << stree.substring(startPosition, length) << " (" << startPosition << ", " << length << ")" << std::endl << std::endl;
-
-        std::cout << "Preprocessing time: " << preprocessingTime << std::endl;
-        std::cout << "Query init. time:   " << queryInitTime << std::endl;
-        std::cout << "Total query time:   " << queryTime << std::endl;
+        handleRepeatQuery(argv);
     } else {
         std::cout << "Unknown query choice." << std::endl;
         return 1;
